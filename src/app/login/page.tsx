11fa53208a231e792415e303aa7cc086eb40e497
carolllinenascimento21-coder <carollline.nascimento21@gmail.com>
2025-12-12 20:27:22 -0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useState } from 'react'
+import { createSupabaseBrowserClient } from "@/lib/supabase";
+const supabase = createSupabaseBrowserClient();import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
