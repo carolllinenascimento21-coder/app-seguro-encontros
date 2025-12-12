c027530192acdfca7e7e3be45b83f63ba3b8a51e
carolllinenascimento21-coder <carollline.nascimento21@gmail.com>
2025-12-12 20:53:35 -0300

    Update page.tsx

--- src/app/minhas-avaliacoes/editar/[id]/page.tsx
+++ src/app/minhas-avaliacoes/editar/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { createSupabaseBrowserClient } from "@/lib/supabase";
-const supabase = createSupabaseBrowserClient();
+import { createClient } from "@/lib/supabase";
+const supabase = createClient();
 import { useState, useEffect } from 'react';
 import { useRouter, useParams } from 'next/navigation';
 import { ArrowLeft, Star, AlertTriangle, Save, Loader2 } from 'lucide-react';
