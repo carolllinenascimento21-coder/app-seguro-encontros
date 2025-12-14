c9b244b234edc85c31c14723a01b1f62a19b354c
carolllinenascimento21-coder <carollline.nascimento21@gmail.com>
2025-12-14 12:04:40 -0300

    Update page.tsx

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { createClient } from '@/lib/supabase/client'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
+import { supabase } from '@/lib/supabase'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -16,7 +16,7 @@ export default function CadastroPage() {
     email: '',
     senha: ''
   })
-  const supabase = createClient()
+  
   const [errors, setErrors] = useState({
     nome: '',
     email: '',
