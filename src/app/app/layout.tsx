6729e4d099dbc726de8403f024d045437a4c712f
carolllinenascimento21-coder <carollline.nascimento21@gmail.com>
2026-02-06 15:09:06 -0300

    Update layout.tsx

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -3,18 +3,10 @@
 import OnboardingGuard from '@/components/custom/onboarding-guard'
 import HeaderCredits from '@/components/HeaderCredits'
 
-export default function AppLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function AppLayout({ children }) {
   return (
-    <OnboardingGuard>
-      <div className="fixed top-4 right-4 z-50">
-       // <HeaderCredits />
-      </div>
-
+    <>
       {children}
-    </OnboardingGuard>
+    </>
   )
 }
